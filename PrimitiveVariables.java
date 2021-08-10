c1ea81ecd9c8e2fca31dbf307ced19e6faef8fa9
pennypesh <48815910+pennypesh@users.noreply.github.com>
2021-08-10 12:46:26 -0500

    initial commit

---
+++ PrimitiveVariables.java
@@ -0,0 +1,22 @@
+public class PrimitiveVariables{
+
+
+
+    public static void main(String...args){
+        System.out.println("Primitive variable app has started! ");
+
+        char singleCharacterVariable = 'A';
+        System.out.println("char value = "+singleCharacterVariable);
+
+        short shortVariable = 2000;
+        System.out.println("short value = "+shortVariable);
+
+        int integerVariable = 500;
+        System.out.println("int value = "+integerVariable);
+
+
+
+
+    
+    }
+}
---
+++ WrapperVariables.java
@@ -0,0 +1,13 @@
+public class WrapperVariables{
+    public static void main (String...args){
+        System.out.println("my name");
+        Character characterValue = 'A';
+        System.out.println("Wrapper Character:"+characterValue);
+    }
+
+
+
+
+    
+
+}
